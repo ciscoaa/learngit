750f661564fd99a83eca2e8e4d22dcd6c94ee316
ciscoaa <1321137425@qq.com>
2020-05-11 15:21:53 +0800

    wrote a python file

---
+++ hello.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+#@Time  : 2020/3/2 13:00
+#@Author: hhq
+#@File  : hello.py
+def say_hello():
+    print('hello 1')
+    print('hello 2')
+    print('hello 3')
+say_hello()
